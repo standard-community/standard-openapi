823620d695deb8b2936f527785e539512647299d
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-03-15 12:56:47 +0530

    test: added vitest

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { StandardSchemaV1 } from "@standard-schema/spec";
 
-export const toOpenAPISchema = (_schema: StandardSchemaV1) => {
+export const toOpenAPISchema = async (_schema: StandardSchemaV1) => {
   return {};
 };
---
+++ tests/index.test.ts
@@ -0,0 +1,43 @@
+import { type } from "arktype";
+import { describe, expect, it } from "vitest";
+import * as v from "valibot";
+import * as z from "zod";
+
+import { toOpenAPISchema } from "../src";
+
+describe("basic", () => {
+  it("arktype", async () => {
+    const schema = type({
+      myString: "string",
+      myUnion: "number | boolean",
+    }).describe("My neat object schema");
+
+    const jsonSchema = await toOpenAPISchema(schema);
+    expect(jsonSchema).toEqual({});
+  });
+
+  it("valibot", async () => {
+    const schema = v.pipe(
+      v.object({
+        myString: v.string(),
+        myUnion: v.union([v.number(), v.boolean()]),
+      }),
+      v.description("My neat object schema"),
+    );
+
+    const jsonSchema = await toOpenAPISchema(schema);
+    expect(jsonSchema).toEqual({});
+  });
+
+  it("zod", async () => {
+    const schema = z
+      .object({
+        myString: z.string(),
+        myUnion: z.union([z.number(), z.boolean()]),
+      })
+      .describe("My neat object schema");
+
+    const jsonSchema = await toOpenAPISchema(schema);
+    expect(jsonSchema).toEqual({});
+  });
+});
